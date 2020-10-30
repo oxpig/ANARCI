93bf0a28d5a9466ab0d019438f91cb9e24d3c186
Piotr Deszyński <deszynski@red-sky.pl>
2020-10-30 09:08:13 +0100

    Automatic install from package managers
    Moved install into setup, so it will run when added through package managers. This allows adding anarci as a depencency to other projects

--- setup.py
+++ setup.py
@@ -5,12 +5,51 @@
 # Maintained by members of OPIG #
 #                               #
 
-import shutil, os, subprocess, imp
+import shutil, os, subprocess, importlib
+
 # Clean this out if it exists
 if os.path.isdir("build"):
     shutil.rmtree("build/")
 
 from distutils.core import setup
+from setuptools.command.install import install
+
+
+def download_files():
+    try:
+        ANARCI_LOC = os.path.dirname(importlib.util.find_spec("anarci").origin)
+    except:
+        sys.stderr.write("Something isn't right. Aborting.")
+        sys.exit(1)
+
+    os.chdir("build_pipeline")
+
+    try:
+        shutil.rmtree("curated_alignments/")
+        shutil.rmtree("muscle_alignments/")
+        shutil.rmtree("HMMs/")
+        shutil.rmtree("IMGT_sequence_files/")
+        os.mkdir(os.path.join(ANARCI_LOC, "dat"))
+    except OSError:
+        pass
+
+    print('Downloading germlines from IMGT and building HMMs...')
+    proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    o, e = proc.communicate()
+
+    print(o.decode())
+    print(e.decode())
+
+    shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
+    shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
+
+
+class Install(install):
+
+    def run(self):
+        super().run()
+        download_files()
+
 
 setup(name='anarci',
       version='1.3',
@@ -26,37 +65,9 @@ setup(name='anarci',
       #                         'dat/HMMs/ALL.hmm.h3m',
       #                         'dat/HMMs/ALL.hmm.h3p']},
       scripts=['bin/ANARCI'],
-      data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS']) ]
+      install_requires=['biopython>=1.78'],
+      data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS']) ],
+      cmdclass={'install': Install}
      )
 
-####
-import sys
-if sys.argv[1] != "install":
-    sys.exit(0)
 
-try:
-    ANARCI_LOC = imp.find_module("anarci")[1]
-except:
-    sys.stderr.write("Something isn't right. Aborting.")
-    sys.exit(1)
-
-os.chdir("build_pipeline")
-
-try:
-    shutil.rmtree("curated_alignments/")
-    shutil.rmtree("muscle_alignments/")
-    shutil.rmtree("HMMs/")
-    shutil.rmtree("IMGT_sequence_files/")
-    os.mkdir(os.path.join(ANARCI_LOC, "dat"))
-except OSError:
-    pass
-
-print('Downloading germlines from IMGT and building HMMs...')
-proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-o, e = proc.communicate()
-
-print(o.decode())
-print(e.decode())
-
-shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
-shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )

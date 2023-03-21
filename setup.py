3a3c0924a6b3a63ca91cfd70fdf93cf567ed09c4
broncio123 <ba13026@my.bristol.ac.uk>
2023-03-21 15:39:17 +0000

    edited setup.py to run build_pipeline

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-
-#                               #
-# Developed by James Dunbar     #
-# Maintained by members of OPIG #
-#                               #
+#! /usr/bin/env python
 
 import shutil, os, subprocess, importlib
 # Clean this out if it exists
@@ -20,36 +15,14 @@ setup(name='anarci',
       url='http://opig.stats.ox.ac.uk/webapps/ANARCI',
       packages=['anarci'], 
       package_dir={'anarci': 'lib/python/anarci'},
-      #package_data={'anarci': ['dat/HMMs/ALL.hmm',
-      #                         'dat/HMMs/ALL.hmm.h3f',
-      #                         'dat/HMMs/ALL.hmm.h3i',
-      #                         'dat/HMMs/ALL.hmm.h3m',
-      #                         'dat/HMMs/ALL.hmm.h3p']},
-      scripts=['bin/ANARCI'],
-      data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS']) ]
+      data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS', 'bin/ANARCI']) ],
+      include_package_data = True,
+      scripts=['bin/ANARCI']
      )
 
-####
-import sys
-if sys.argv[1] != "install":
-    sys.exit(0)
 
-try:
-    ANARCI_LOC = importlib.util.find_spec("anarci").submodule_search_locations[0]
-except Exception as e:
-    sys.stderr.write(e.message)
-    sys.exit(1)
-
+ANARCI_LOC = importlib.util.find_spec("anarci").submodule_search_locations[0]
 os.chdir("build_pipeline")
-
-try:
-    shutil.rmtree("curated_alignments/")
-    shutil.rmtree("muscle_alignments/")
-    shutil.rmtree("HMMs/")
-    shutil.rmtree("IMGT_sequence_files/")
-    os.mkdir(os.path.join(ANARCI_LOC, "dat"))
-except OSError:
-    pass
 
 print('Downloading germlines from IMGT and building HMMs...')
 proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
@@ -60,3 +33,13 @@ print(e.decode())
 
 shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
 shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
+
+try:
+    shutil.rmtree("curated_alignments/")
+    shutil.rmtree("muscle_alignments/")
+    shutil.rmtree("HMMs/")
+    shutil.rmtree("IMGT_sequence_files/")
+    os.mkdir(os.path.join(ANARCI_LOC, "dat"))
+except OSError:
+    pass
+

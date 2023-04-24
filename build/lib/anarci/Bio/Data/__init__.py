0a2a4fe6feb6e67f7f332b9abaabb3809f5c4ef8
Broncio Aguilar Sanjuan <sanjuan@dragon000.stats.ox.ac.uk>
2023-04-24 17:10:34 +0100

    added gitignore rules and deleter build files

--- build/lib/anarci/Bio/Data/__init__.py
+++
@@ -1,5 +0,0 @@
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
-
-"""Collections of various bits of useful biological data."""

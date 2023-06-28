38b45154055da30125a8319bd7f04805792ab5e5
broncio123 <ba13026@my.bristol.ac.uk>
2023-06-28 14:01:16 +0100

    replace python3 with python call

--- build_pipeline/RUN_pipeline.sh
+++ build_pipeline/RUN_pipeline.sh
@@ -5,12 +5,12 @@ DIR="."
 # Rip the sequences from the imgt website. HTML may change in the future. 
 mkdir -p $DIR/IMGT_sequence_files/htmlfiles
 mkdir -p $DIR/IMGT_sequence_files/fastafiles
-python3 $DIR/RipIMGT.py
+python $DIR/RipIMGT.py
 
 # Format the alignments and handle imgt oddities to put into a consistent alignment format
 mkdir -p $DIR/curated_alignments
 mkdir -p $DIR/muscle_alignments
-python3 $DIR/FormatAlignments.py
+python $DIR/FormatAlignments.py
 
 # Build the hmms for each species and chain.
 # --hand option required otherwise it will delete columns that are mainly gaps. We want 128 columns otherwise ARNACI will fall over.
